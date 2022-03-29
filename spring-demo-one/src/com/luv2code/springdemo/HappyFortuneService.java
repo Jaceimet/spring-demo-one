dafd1fd13cd5ee27c1b39191293857f7ea30a061
Jaceimet <84209692+Jaceimet@users.noreply.github.com>
2022-03-28 19:14:15 -0700

    Assignment #2 added fortune array and injected new dependency into the coach implementation

--- spring-demo-one/src/com/luv2code/springdemo/HappyFortuneService.java
+++ spring-demo-one/src/com/luv2code/springdemo/HappyFortuneService.java
@@ -1,11 +1,17 @@
 package com.luv2code.springdemo;
 
+import java.util.Random;
+
 public class HappyFortuneService implements FortuneService {
 
+	private Random random = new Random();
+	
 	@Override
 	public String getFortune() {
-
-		return "Today is your lucky day!";
+		
+		int fortuneNumber = random.nextInt(fortuneArray.length);
+		
+		return "Todays fortune number is #" +fortuneNumber+ ": "+ fortuneArray[fortuneNumber];
 	}
 
 }
